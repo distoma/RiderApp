4887e28deba00e010c51d9a95e991fe6545690b6
jinseokpark <distoma@gmail.com>
2023-02-08 15:09:26 +0900

    add MapView pin to distoma's House

---
+++ RiderApp/MapView.swift
@@ -0,0 +1,61 @@
+//
+//  MapView.swift
+//  RiderApp
+//
+//  Created by jinseok PARK on 2023/02/08.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+        
+    
+    var centerCoordinate: CLLocationCoordinate2D
+    var mapAnnotations: [MKPointAnnotation]
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+//        mapView.setCenter(CLLocationCoordinate2DMake(37.334903, -122.008687), animated: true)
+        mapView.setCenter(CLLocationCoordinate2DMake(35.22994, 128.50671), animated: true)
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        if mapAnnotations.count != uiView.annotations.count {
+            uiView.removeAnnotations(uiView.annotations)
+            uiView.addAnnotations(mapAnnotations)
+            
+        }
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var mapViewParent: MapView
+        init (_ parent: MapView) {
+            self.mapViewParent = parent
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            mapViewParent.centerCoordinate = mapView.centerCoordinate
+        }
+    }
+}
+
+extension MKPointAnnotation {
+    static var appleParkExample: MKPointAnnotation {
+        let annotation = MKPointAnnotation()
+        annotation.title = "distoma's House"
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 35.22994, longitude: 128.50671)
+        return annotation
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView(centerCoordinate: MKPointAnnotation.appleParkExample.coordinate, mapAnnotations: [MKPointAnnotation.appleParkExample])
+    }
+}
