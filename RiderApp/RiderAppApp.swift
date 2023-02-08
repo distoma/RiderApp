26109d824b1a8db11d751fd3adebc721c242f9dc
jinseokpark <distoma@gmail.com>
2023-02-08 10:10:56 +0900

    Initial Commit

---
+++ RiderApp/RiderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiderAppApp.swift
+//  RiderApp
+//
+//  Created by jinseok PARK on 2023/02/08.
+//
+
+import SwiftUI
+
+@main
+struct RiderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
